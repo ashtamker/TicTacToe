0e3bb171571b463583d2bbbd8771877c0f4e3649
Or Ashtamker <ashi@MacBook-Pro.local>
2021-03-22 15:41:30 +0200

    add icon for player

---
+++ tic-tac-toe/src/components/Shiled.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../icon/Shield.svg';
+
+export default function Shiled(props) {
+    const size = props.size || 100;
+
+
+    return (
+        <svg>
+            Shiled
+        </svg>
+    )
+} 
