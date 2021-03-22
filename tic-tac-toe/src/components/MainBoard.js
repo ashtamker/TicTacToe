fc319ce676e76fb846e51a27772d5a42e5d8484f
Or Ashtamker <ashi@MacBook-Pro.local>
2021-03-22 15:17:40 +0200

    make the board

---
+++ tic-tac-toe/src/components/MainBoard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import  '../App.css';
+
+class MainBoard extends React.Component {
+
+    
+    
+    render() {
+        return (
+           <div className= "container">
+               <div />
+               <div />
+               <div />
+               <div />
+               <div />
+               <div />
+               <div />
+               <div />
+               <div />
+           </div> 
+        )
+    }
+
+}
+
+
+export default MainBoard;
